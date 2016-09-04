4fbd3e0b6ed68eda699b7406a2701c23f3fba2f4
Suada Haji <suadahaji@Suadas-MacBook-Pro-2.local>
2016-09-04 16:39:47 +0300

    Create usersController responsible for creating, listing, updating and deleting users

---
+++ server/controllers/user.js
@@ -0,0 +1,71 @@
+const User = require('../models').User;
+
+module.exports = {
+  create(req, res) {
+    return User
+      .create({
+        username: req.body.username,
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+      })
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error));
+  },
+  list(req, res) {
+    return User
+      .all()
+      .then(user => res.status(200).send(user))
+      .catch(error => res.status(400).send(error));
+  },
+  retrieve(req, res) {
+    return User
+      .findById(req.params.userId, {})
+      .then(user => {
+        if (!user) {
+          return res.status(404).send({
+            message: 'User Not Found',
+          });
+        }
+        return res.status(200).send(user);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  update(req, res) {
+    return User
+      .findById(req.params.userId, {})
+      .then(user => {
+        if (!user) {
+          return res.status(404).send({
+            message: 'User Not Found',
+          });
+        }
+        return user
+          .update({
+            username: req.body.username || user.username,
+            fname: req.body.fname || user.fname,
+            lname: req.body.lname || user.lname,
+            email: req.body.email || user.email,
+          })
+          .then(() => res.status(200).send(user))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+  destroy(req, res) {
+  return User
+    .findById(req.params.userId)
+    .then(user => {
+      if (!user) {
+        return res.status(400).send({
+          message: 'User Not Found',
+        });
+      }
+      return user
+        .destroy()
+        .then(() => res.status(204).send())
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+},
+};
